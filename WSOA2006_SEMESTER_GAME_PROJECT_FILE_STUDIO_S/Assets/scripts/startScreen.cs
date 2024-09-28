68b46b7fc403ae57e2d247e8d303e1eadc6d7fc3
jordanchicksen1 <2333640@students.wits.ac.za>
2024-09-28 20:46:57 +0200

    added more title screen stuffs

--- WSOA2006_SEMESTER_GAME_PROJECT_FILE_STUDIO_S/Assets/scripts/startScreen.cs
+++ WSOA2006_SEMESTER_GAME_PROJECT_FILE_STUDIO_S/Assets/scripts/startScreen.cs
@@ -11,6 +11,13 @@ public class startScreen : MonoBehaviour
     public AudioClip page1;
     public AudioClip tvSFX;
     public AudioClip beginSFX;
+
+    //title screen positio info x:-452.03 y:1 z:-579.9
+
+    //chapterSpotLights
+    public GameObject spotLight1;
+    public GameObject spotLight2;
+    public GameObject spotLight3;
     public void Play()
     {
         
@@ -56,5 +63,26 @@ public class startScreen : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         camera1.transform.position = new Vector3(-451.7f, -710f, -579.9f);
+    }
+
+    public void chapterSelect()
+    {
+        camera1.transform.position = new Vector3(2010f, -712f, -579.9f);
+        pageTurner.clip = page1;
+        pageTurner.Play();
+    }
+
+    public IEnumerator ChapterOneSelect()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("dayna_fuckaround");
+    }
+
+    public void chapterOne()
+    {
+        StartCoroutine(ChapterOneSelect());
+        pageTurner.clip = tvSFX;
+        pageTurner.Play();
+        spotLight1.SetActive(true);
     }
 }
